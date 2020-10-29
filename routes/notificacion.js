c7b9434badaabc65fdd691bbeeb64b0b5c3ccbf1
Maxi <arrieta.maxi18@gmail.com>
2020-10-29 16:44:02 -0300

    solucion al push de notificaciones

--- routes/notificacion.js
+++ routes/notificacion.js
@@ -1,41 +1,41 @@
-const express = require("express");
-const fetch = require("node-fetch");
-const moment = require("moment");
+const express = require('express');
+const fetch = require('node-fetch');
+const moment = require('moment');
 const app = express();
 const arrayNotificaciones = [];
-moment.locale("es");
+moment.locale('es');
 // let opciones = { hour: 'numeric', minute: '2-digit', hour12: false };
 // Esta funcion se ejecuta cada 30 segundas y reccore el array de notificaciones
 // las copara y quita las repetidas xq se envian push todo el timepo si hay alguna
 // repetida la saca, si todo esta ok envia la notificacion
 const sendNotificacion = () => {
-    let dia = new Date().toLocaleString("es-AR", {
-        timeZone: "America/Buenos_Aires",
+    let dia = new Date().toLocaleString('es-AR', {
+        timeZone: 'America/Buenos_Aires',
     });
-    let hora = moment(dia).format("LT");
+    let hora = moment(dia).format('LT');
 
     if (arrayNotificaciones.length === 0) return;
     arrayNotificaciones.map((notificacion, index) => {
-        console.log("///// Hora ///// Notificacion /////");
+        console.log('///// Hora ///// Notificacion /////');
         console.log(`   ${hora}    ===    ${notificacion.time}`);
         // Comparo la hora de la notificacion con la hora
         // si son iguales se ejecuta la notificacion
         if (hora === notificacion.time) {
-            console.log("Son iguales ");
-            fetch("https://exp.host/--/api/v2/push/send", {
-                    method: "post",
+            console.log('Son iguales ');
+            fetch('https://exp.host/--/api/v2/push/send', {
+                    method: 'post',
                     headers: {
-                        Accept: "application/json",
-                        "Accept-Encoding": "gzip, deflate",
-                        "Content-Type": "application/json",
+                        Accept: 'application/json',
+                        'Accept-Encoding': 'gzip, deflate',
+                        'Content-Type': 'application/json',
                     },
                     body: JSON.stringify(notificacion.mensaje),
                 })
                 .then((res) => res.json())
                 .then((resultado) => {
-                    console.log("Resultado: ", resultado);
+                    console.log('Resultado: ', resultado);
                     // Si esta todo ok sacamos la notificacion del array
-                    if (resultado.data.status === "ok") {
+                    if (resultado.data.status === 'ok') {
                         arrayNotificaciones.splice(index, 1);
                     } else {
                         // si hay un error no puedo hacer nada de aca
@@ -44,37 +44,37 @@ const sendNotificacion = () => {
                     }
                 });
         } else {
-            console.log("son distintos ");
+            console.log('son distintos ');
         }
     });
 };
 
 // Creo la ruta que resive los datos con el contenido de la notificacion
-app.post("/notificacion", (req, res) => {
+app.post('/notificacion', (req, res) => {
     let body = req.body;
     // Hago destructuracion del body con todo lo que necesito
     const { token, titulo, contenido, usuario, time } = body;
     // Hago validacion de campos vacios
     if (
-        token.trim() === "" ||
-        titulo.trim() === "" ||
-        contenido.trim() === "" ||
-        usuario.trim() === "" ||
-        time === ""
+        token.trim() === '' ||
+        titulo.trim() === '' ||
+        contenido.trim() === '' ||
+        usuario.trim() === '' ||
+        time === ''
     ) {
         res.json({
             ok: false,
             err: {
-                message: "Todos los campos son obligatorios",
+                message: 'Todos los campos son obligatorios',
             },
         });
     } else {
         // Creo la variable con todos los datos del mensaje
         let mensaje = {
             to: token,
-            data: { extraData: "Some data" },
+            data: { extraData: 'Some data' },
             title: titulo,
-            body: contenido + " " + usuario,
+            body: contenido + ' ' + usuario,
             ttl: 2419200,
             badge: 0,
         };
@@ -84,7 +84,7 @@ app.post("/notificacion", (req, res) => {
         // Respuesta de la api
         res.json({
             ok: true,
-            message: "Se enviaron los datos correctamente",
+            message: 'Se enviaron los datos correctamente',
         });
         // lo mando a comparar como objeto asi esta igual que la notificacion
         comparar({ mensaje, time });
@@ -93,6 +93,7 @@ app.post("/notificacion", (req, res) => {
 
 const comparar = (nuevaNotificacion) => {
     // hacemos un if para saber si hay mas de 1 notificaciones agregadas
+    let estaEnElArray = false;
     if (arrayNotificaciones.length > 0) {
         // si hay mas de 1 entonces lo recorre
         arrayNotificaciones.map((notificacion) => {
@@ -102,23 +103,26 @@ const comparar = (nuevaNotificacion) => {
                 if (notificacion.mensaje.body === nuevaNotificacion.mensaje.body) {
                     // y por ultimo por la hora, si todo esto coincide
                     if (notificacion.time === nuevaNotificacion.time) {
-                        // saco la notificacion del array
-
-                        console.log("***************************************************");
-                        console.log(notificacion.time, " === ", nuevaNotificacion.time);
-                        console.log("No se agrego, ya esta en el array");
-                    } else {
-                        arrayNotificaciones.push(nuevaNotificacion);
+                        // Si es true es xq esta en el array entonces
+                        // paso el estaEnElArray como true
+                        estaEnElArray = true;
+                        console.log('***************************************************');
+                        console.log(notificacion.time, ' === ', nuevaNotificacion.time);
+                        console.log('No se agrego, ya esta en el array');
                     }
-                } else {
-                    arrayNotificaciones.push(nuevaNotificacion);
                 }
-            } else {
-                arrayNotificaciones.push(nuevaNotificacion);
             }
         });
-    } else {
+    }
+
+    // Si estaEnElArray es false, lo agrogo al array
+    // si es treu es xq ya esta entonces no lo agrego xq sino
+    // lo tengo 2 veces
+    if (!estaEnElArray) {
         arrayNotificaciones.push(nuevaNotificacion);
+        console.log('Se agrega');
+    } else {
+        console.log('No se agrega');
     }
 };
 
